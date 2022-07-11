0a403220918a39416548977bfd46eb872880549e
TheSourGuy <sourishw@gmail.com>
2022-07-11 10:22:36 -0500

    Just Before Submitting

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./authConfig";
+import { Provider } from "react-redux/es/exports";
+import store from "./store";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <MsalProvider instance={msalInstance}>
-      <App />
-    </MsalProvider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <MsalProvider instance={msalInstance}>
+        <App />
+      </MsalProvider>
+    </React.StrictMode>
+  </Provider>,
+
   document.getElementById("root")
 );
